17fc7a3b7c24546f8e2248380e22d4fc0e32f7a1
JasdeepN <jasdeep4@hotmail.com>
2016-02-16 14:41:55 -0500

    works without comparator

--- assignments/a1/a1q2.scala
+++ assignments/a1/a1q2.scala
@@ -1,6 +1,20 @@
-object quicksort{
-	def main(args: Array[String]): Unit = {
-
+object sort{
+	def quicksort(input: List[Int]): List[Int] = {
+		if (input.length <= 1) input
+		else {
+			val pivot = input(input.length / 2)
+			List.concat(
+				quicksort(input filter (pivot >)),
+				input filter (pivot ==),
+				quicksort(input filter (pivot <)))
+		}
 	}
 
+
+
+	def main(args: Array[String]): Unit = {
+		println("welcome to quicksort")
+		var list = List.tabulate(50)( _ => (Math.random*10).toInt )
+		println(quicksort(list))
+	}
 }
