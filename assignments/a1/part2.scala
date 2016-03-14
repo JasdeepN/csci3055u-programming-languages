21e7ec1d9db1780d903134774da1db8e222078f6
JasdeepN <jasdeepn4@hotmail.com>
2016-03-14 12:32:34 -0400

    updating files

--- assignments/a1/part2.scala
+++ assignments/a1/part2.scala
@@ -8,20 +8,13 @@ object sort {
 				quicksort[K](comparator)(input.filter(comparator(_, pivot) > 0)),
 				(input.filter(comparator(_, pivot) == 0)),
 				quicksort[K](comparator)(input.filter(comparator(_, pivot) < 0)))
-
 		}
-	}
-
-	def comparator[Integer](a: Int, b: Int): Int ={
-		if (a > b) -1
-		else if (b > a) 1
-		else 0
 	}
 
 	def main(args: Array[String]): Unit = {
 		println("generates 100 random integers from 1 to 1000 and quicksorts them")
 		var list = List.tabulate(100)( _ => Math.ceil(Math.random() * 1000).toInt)
-		var sorted_list = (quicksort[Int](comparator[Integer])(list))
+		var sorted_list = (quicksort((x: Int, y: Int) => {if (x < y) -1 else if (x > y) 1 else 0})(list))
 		println(sorted_list)
 	}
 }
